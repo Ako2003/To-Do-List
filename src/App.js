143b4ccd12cca591a1bd6d71132cfd5eb9600fbf
Ako2003 <akifmursalov@gmail.com>
2024-03-20 06:04:25 -0700

    Final version

--- src/App.js
+++ src/App.js
@@ -23,12 +23,14 @@ export default function App(){
       <section>
         {items.map((item, index) => (
           <div className="bg-[#496989] text-white rounded-xl text-left mb-5 pl-3 py-3 flex justify-between">
-            <strong>{index + 1}.</strong>
-            <input 
-              className="bg-transparent outline-transparent w-36" 
-              value={item} 
-              disabled
-            />
+            <div>
+              <strong>{index + 1}. </strong>
+              <input 
+                className="bg-transparent outline-transparent w-36 text-white font-bold" 
+                value={item} 
+                disabled
+              />
+            </div>
             <div className="flex">
               <img alt="delete-icon" width={36} onClick={() => handleDelete(index)} className="pr-3 cursor-pointer" src="./delete.svg"/>
             </div>
