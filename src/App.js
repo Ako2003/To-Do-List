86767d1f1f8446f31236a7e425dc1936a3ba34ae
Ako2003 <akifmursalov@gmail.com>
2024-03-20 05:52:35 -0700

    Final version

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import  Input from "./components/Input"
 import "./App.css"
 
 export default function App(){
-  const [items, setItems] = useState(['Ilqar','Fatima','Akif','Ravan','Leyla']);
-
+  const [items, setItems] = useState(['JavaScript','Python','Java','GO','C/C++','C#','PHP']);
+  
   const handleDelete = (index) => {
    const updatedItems = items.filter((item, idx) => idx !== index);
    setItems(updatedItems)
   }
- 
+
   return(
-    <div className="grid justify-items-center nunito">
+    <div className="grid justify-items-center nunito mb-7">
       <section className="mt-20 mb-10">
-        <span className="text-5xl">
+        <span className="text-5xl text-[#A8CD9F]">
           Todo List
         </span>
       </section>
@@ -22,9 +22,16 @@ export default function App(){
       </section>
       <section>
         {items.map((item, index) => (
-          <div className="border border-[#585858] text-[#585858] rounded-xl text-left mb-5 pl-3 py-3 flex justify-between">
-            <p className="pr-28"><strong>{index + 1}.</strong> {item}</p>
-            <img onClick={() => handleDelete(index)} className="pr-3 cursor-pointer" src="./delete.png"/>
+          <div className="bg-[#496989] text-white rounded-xl text-left mb-5 pl-3 py-3 flex justify-between">
+            <strong>{index + 1}.</strong>
+            <input 
+              className="bg-transparent outline-transparent w-36" 
+              value={item} 
+              disabled
+            />
+            <div className="flex">
+              <img alt="delete-icon" width={36} onClick={() => handleDelete(index)} className="pr-3 cursor-pointer" src="./delete.svg"/>
+            </div>
           </div>
         ))}
       </section>
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,9 +1,7 @@
 import React, { useState } from "react";
 
 export default function Input({ items, setItems }){
-
     const [value, setValue] = useState('');
-
     const handleItem = (e) => {
         e.preventDefault()
         setItems([
@@ -13,16 +11,30 @@ export default function Input({ items, setItems }){
         setValue('')
     }
 
+    const handleDelete = () => {
+        setItems([])
+    }
+
     return (
         <div>
-            <form onSubmit={(e) => handleItem(e)}>
-                <input 
-                    className="border border-black rounded-s p-3"
-                    value={value}
-                    onChange={e => setValue(e.target.value)}
-                    required
-                />
-                <button className="border border-black p-3">Submit</button>
+            <form className="flex flex-col gap-3" onSubmit={(e) => handleItem(e)}>
+                <section className="flex flex-col">
+                    <label className="absolute text-xs mx-3 my-2 text-white font-bold">Enter any text</label>
+                    <input 
+                        className="outline-transparent border-transparent rounded-xl text-white p-3 pt-7 bg-[#58A399]"
+                        value={value}
+                        onChange={e => setValue(e.target.value)}
+                        required
+                    />
+                </section>
+                <button className="outline-transparent border-transparent font-bold rounded-xl p-3 bg-[#58A399] text-white">Submit</button>
+                <button
+                    type="button" 
+                    onClick={() => handleDelete()} 
+                    className={`${items.length === 0 ? 'bg-[#8dc3bb]':'bg-[#58A399]'}  outline-transparent border-transparent font-bold rounded-xl p-3 text-white`}
+                    disabled={items.length === 0}
+                    >Delete All</button>
+
             </form>
         </div>
     )
